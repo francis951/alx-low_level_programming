7f31e51bc726a47872ecb6641a264a2e4d33be64
francis951 <francis_951@live.com>
2023-07-21 02:44:44 -0700

    corrected the spelling error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,21 +4,18 @@
  * main - causes an infinite loop
  *
  * Return: 0
- *
  */
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming : (\n)");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	/*while (i < 10)*/
+	while (i < 10)
 	{
-		/*putchar(i);*/
+		putchar(i);
 	}
-
 	printf("Infinite loop avoided! \\0/\n");
 	return (0);
 }
