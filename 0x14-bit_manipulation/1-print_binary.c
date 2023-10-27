0c367aa9af1332527d6ce9612021e78e4c5c2477
Francis Murungi <francis_951@live.com>
2023-10-27 01:53:01 -0700

    add bit-manipulations

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - equivalent of a decimal number that prints a binary
+ * @n: the amount or number binary to be printed
+ */
+
+void print_binary(unsigned long int n)
+{
+        int lea, mann = 0;
+        unsigned long int new;
+
+        for (lea = 63; lea >= 0; lea--)
+        {
+                new = n >> lea;
+                if (new & 1)
+                {
+                        _putchar('1');
+                        mann++;
+                }
+                else if (new)
+                        _putchar('0');
+        }
+        if (!new)
+                _putchar('0');
+}
